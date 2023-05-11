8d985bb98cc6e2d96faf302b7b9308989f61908f
masal9pse <returnymgstokh@icloud.com>
2023-05-12 05:25:51 +0900

    repositoryの使い回し

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:use_state_notifier/counter_controller.dart';
 import 'package:use_state_notifier/counter_state.dart';
 import 'package:use_state_notifier/counter_view.dart';
 
 void main() {
-  runApp(StateNotifierProvider<CounterController,CounterState>(
-    create: (context) => CounterController(CounterRepository()),
-    // child: CounterView(),
-    child: MyApp(),
-    //  {
-    //   return const MyApp();
-    // }
-  ));
+  runApp(
+    Provider(
+      create: (context) => CounterRepository(),
+      child: StateNotifierProvider<CounterController, CounterState>(
+        create: (context) =>
+            CounterController(context.read<CounterRepository>()),
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,21 +27,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -119,9 +107,14 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const CounterView()));
-            }, child: Text('navigate')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const CounterView()));
+                },
+                child: Text('navigate')),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
